aa55fa816548cba4cef34e4244b10fc04e2d426a
Jose Nieto <josenietooso@gmail.com>
2026-06-12 20:36:36 -0300

    Agregue el ejercicio 1202

---
+++ src/ejercicios/ejercicio1202.py
@@ -0,0 +1,8 @@
+"""Este programa solicita ingresar un numero entero e informar si el
+valor leido es mayo o menor que 10"""
+
+num=int(input("Ingrese un numero entero: "))
+if num > 10:
+    print("El Numero es mayor que 10")
+else: 
+    print("El Numero es menor que 10")
