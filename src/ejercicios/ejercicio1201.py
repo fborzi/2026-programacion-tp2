69dfceb223b8e67a836c26b6a5dcd1978be60dbe
Jose Nieto <josenietooso@gmail.com>
2026-06-12 19:58:24 -0300

    Arregle problema de division por 0 en el ejercicio 1201

--- src/ejercicios/ejercicio1201.py
+++ src/ejercicios/ejercicio1201.py
@@ -10,9 +10,14 @@ num_2=int(input("Ingrese el segundo numero entero: "))
 suma = num_1 + num_2
 resta = num_1 - num_2
 multiplicacion = num_1 * num_2
-cociente = num_1 / num_2
-resto = num_1 % num_2
-division_entera = num_1 // num_2
+if num_2 != 0:
+    cociente = num_1 / num_2
+    resto = num_1 % num_2
+    division_entera = num_1 // num_2
+else:
+    cociente = "No se puede dividir por cero" 
+    resto = "No se puede dividir por cero"
+    division_entera = "No se puede dividir por cero"
 absoluto_1 = abs(num_1)
 absoluto_2 = abs(num_2)
 
@@ -20,7 +25,7 @@ absoluto_2 = abs(num_2)
 print("La suma de los dos numeros es: ",suma)
 print("La resta del primer numero menos el segundo es: ",resta)
 print("La multiplicacion de los dos numeros es: ",multiplicacion)
-print("La division del primer numerto entre el segundo es: ",cociente)
+print("La division del primer numero entre el segundo es: ",cociente)
 print("El resto de la division del primer numero entre el segundo es: ",resto)
 print("La division entera del primer numero entre el segundo es: ",division_entera)
 print("El valor absoluto del primer numero es: ",absoluto_1)
