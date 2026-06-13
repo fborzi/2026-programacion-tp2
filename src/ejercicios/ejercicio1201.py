82fd379c287cdebbc7e6c19e9c4e85729df68392
Ludmila Ravichoni <ludmiravichoni@gmail.com>
2026-06-12 21:34:30 -0300

    correccion del ejercicio 1201, defini nuevas variables

--- src/ejercicios/ejercicio1201.py
+++ src/ejercicios/ejercicio1201.py
@@ -1,14 +1,16 @@
-SUMA = 0
-RESTA = 0
-DIVISION = 0.0
-MULTIPLICACION = 0.0
+suma = 0
+resta = 0
+division = 0.0
+resto = 0 
+division_entera = 0
+multiplicacion = 0.0
 
 nro1 = int(input("Ingrese el primer numero:"))
 nro2 = int(input("Ingrese el segundo numero:"))
 
 print("La suma de los dos numeros es:",nro1+nro2)
-print("La resta del prier numero menos el segundo es:", nro1-nro2)
-print("La multiplcacion de los dos numeros es:", nro1 * nro2)
+print("La resta del primer numero menos el segundo es:", nro1-nro2)
+print("La multiplcacion de los dos numeros es:", nro1*nro2)
 print("La division del primer numero entre el segundo es:", nro1/nro2)
 print("El resto de la division del primer numero entre el segundo es:", nro1%nro2)
 print("La division entera del primer numero entre el segundo es:", nro1//nro2)
