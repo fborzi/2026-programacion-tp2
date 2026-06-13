9d0a3e5f57769774120bcf600d82a001fc670a04
vann <ivanjtomicic@gmai.com>
2026-06-12 21:46:52 -0300

    ingresa un número entero y compara si es mayor o menor a 10

---
+++ src/ejercicios/ejercicio1202.py
@@ -0,0 +1,8 @@
+# ingresa un numero entero
+numero=0
+numero = int(input("ingresa el numero entero: "))
+# compara si el numero es mayor o menor a 10
+if numero < 10 :
+    print("el numero es menor a 10")
+elif numero > 10:
+    print("el numero es mayor a 10")
