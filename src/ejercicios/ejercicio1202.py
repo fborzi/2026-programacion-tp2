7a6fa0e11247637695289f3e6b658a2008d54af5
Lautaro Tamola <tamolalautaro@gmail.com>
2026-06-12 21:05:58 -0300

    [Tp-002] - {feat}: se realiza carga del ejercicio 1204 y ademas se agregan comentarios a los ejercicios 1202,1203

--- src/ejercicios/ejercicio1202.py
+++ src/ejercicios/ejercicio1202.py
@@ -1,3 +1,4 @@
+""" En este ejercicio pediremos ingresar un numero entero y mostraremos por pantalla si el numero es mayor a 10 o no"""
 numero1 = int(input("Ingrese primer numero entero:"))
 if numero1 > 10:
     print("El numero es mayor a 10")
---
+++ src/ejercicios/ejercicio1204.py
@@ -0,0 +1,7 @@
+""" en este ejercicio pediremos el ingreso de un numero y calcularemos de forma matematica si el numero es par o
+impar, luego imprimiremos el resultado"""
+numero1 = int(input("Ingrese primer numero entero:"))
+if numero1 % 2 == 0:
+    print("El numero ",numero1, " es par")
+else:
+    print("El numero ",numero1, " es impar") 
