28f195f5567d870889216c65d8973cafbebc23f5
Carola Tala <carolabelentala05@gmail.com>
2026-06-16 22:56:11 -0300

    Realiza operaciones matemáticas básicas entre dos números: suma, resta, multiplicación, división, resto, división entera y valor absoluto

--- src/ejercicios/ejercicio1202.py
+++ src/ejercicios/ejercicio1202.py
@@ -2,5 +2,7 @@ numero = int(input("Ingrese un numero: "))
 
 if numero > 10:
     print("El numero es mayor que 10.")
+elif numero == 10:
+    print("El numero es igual a 10.")
 else:
-    print("El numero no es mayor que 10.")
+    print("El numero no es mayor que 10")
