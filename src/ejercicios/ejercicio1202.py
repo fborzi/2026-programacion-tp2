b1cce639f98240241352e061f647bdd491a3293a
Carola Tala <carolabelentala05@gmail.com>
2026-06-17 00:23:00 -0300

    modifique los mensajes de los print

--- src/ejercicios/ejercicio1202.py
+++ src/ejercicios/ejercicio1202.py
@@ -1,8 +1,8 @@
 numero = int(input("Ingrese un numero: "))
 
 if numero > 10:
-    print("El numero", numero, "es mayor que 10.")
+    print("El numero es mayor que 10.")
 elif numero < 10:
-    print("El numero", numero, "es menor que 10")
+    print("El numero es menor que 10")
 else:
-    print("El numero", numero, "es igual que 10.")
+    print("El numero es igual a 10.")
