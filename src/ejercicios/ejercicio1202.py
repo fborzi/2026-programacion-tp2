69bde25d680e412c3409423e23e1e9bf3daa98c0
Carola Tala <carolabelentala05@gmail.com>
2026-06-16 23:44:58 -0300

    cambie de lugar los mensajes de los utimos print y lo que iba en el elif del medio

--- src/ejercicios/ejercicio1202.py
+++ src/ejercicios/ejercicio1202.py
@@ -2,7 +2,7 @@ numero = int(input("Ingrese un numero: "))
 
 if numero > 10:
     print("El numero es mayor que 10.")
-elif numero == 10:
-    print("El numero es igual a 10.")
-else:
+elif numero < 10:
     print("El numero es menor que 10")
+else:
+    print("El numero es igual a 10.")
