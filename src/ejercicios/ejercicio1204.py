a5330d30e28dba46af70bbc4b6dfe860191f9964
vann <ivanjtomicic@gmai.com>
2026-06-19 20:38:06 -0300

    El codigo verifica si un número es par o impar

---
+++ src/ejercicios/ejercicio1204.py
@@ -0,0 +1,8 @@
+#solicita un número entero al usuario
+numero= int(input("ingresá el número entero "))
+#verifica si es par: el resto de dividir por 2 es 0
+if numero % 2 == 0:
+    print("el número es PAR")
+else:
+    print("el número es IMPAR")
+    
