bae0005f56319707f59b7b72170db6968f5a01df
Jose Nieto <josenietooso@gmail.com>
2026-06-19 20:06:46 -0300

    Modifique la posicion de los print para que cuando se ingrese el numero 0 ya imprima los datos

--- src/ejercicios/ejercicio1209.py
+++ src/ejercicios/ejercicio1209.py
@@ -1,22 +1,22 @@
 """Este programa  solicita el ingreso del numero de alumno y su nota recibida
 hasta que se ingrese un numero de alumno igual a 0,la leyenda debera imprimir la cantidad de alumnos aprobados,
-y la cantidad de alumnos desaprobados, se debera tener en cuenta que se aprueba con una nota mayor a 4."""
+y la cantidad de alumnos desaprobados, se debera tener en cuenta que se aprueba con una nota mayor o igual a 4."""
 
 
 alumno = int(input("Ingrese el numero del alumno: "))
-nota = float(input("Ingrese la nota del alumno: "))
+
 
 aprobados = 0
 desaprobados = 0
 
 while alumno != 0:
-    if nota > 4:
+    nota = float(input("Ingrese la nota del alumno: "))
+    if nota >= 4:
         aprobados = aprobados + 1
     else: 
         desaprobados = desaprobados + 1
        
     alumno = int(input("Ingrese el numero del alumno: "))
-    nota = float(input("Ingrese la nota del alumno: ")) 
 
 
 print("La cantidad de aprobados es : ",aprobados)
