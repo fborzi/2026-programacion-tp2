ee20970115cfb9272fb2d39e4aebf76c08f7f2e7
PC <gasston327@gmail.com>
2026-06-19 20:51:18 -0300

    ejercicio1205 completo. Se ingres un dia de la semana y la cantidad de articulos vendidos a un cliente. Se evalua si el dia es lunes, y si la cantidad de articulos es mayor a 3, para que se imprima si accede a un descuento. Si accede se imprime por pantalla, si no accede, no se imprime nada

---
+++ src/ejercicios/ejercicio1205.py
@@ -0,0 +1,7 @@
+dia=input("Ingrese el dia de la semana:")
+cantArt = int(input("Ingrese la cantidad de articulos:"))
+
+if dia == "lunes" :
+    if cantArt > 3 :
+        print("Accede al descuento")
+    
