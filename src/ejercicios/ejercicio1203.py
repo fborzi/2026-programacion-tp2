31e17b228cd7d5799ee18a8b7b784c42166ad1cc
vann <ivanjtomicic@gmai.com>
2026-06-19 20:07:46 -0300

    el codigo compara si el primer número ingresado es mayor o igual al segundo

---
+++ src/ejercicios/ejercicio1203.py
@@ -0,0 +1,14 @@
+# ingresa dos  numeros enteros
+numero1 = 0
+numero2 = 0
+numero1= int(input("ingresa el  primer numero entero: "))
+numero2= int(input("ingresa el segundo  numero entero: "))
+# compara los dos numeros
+if numero1 < numero2  :
+    print("el  primer numero  es menor al segundo numero ")
+elif numero1 > numero2 :
+    print("el  primer numero  es mayor  al segundo numero")
+elif numero1 == numero2 :
+    print("el  primer numero  es igual  al segundo numero")
+    
+    
