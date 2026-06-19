73f5db5efd1f1265b93a700a5cd0121441a7ecf1
PC <gasston327@gmail.com>
2026-06-19 20:38:46 -0300

    ejercicio1203 completo. Se ingresan dos numeros y se compara si el primero es mayor, menor o igual al segundo. Se imprime por pantalla la respuesta

---
+++ src/ejercicios/ejercicio1203.py
@@ -0,0 +1,9 @@
+numero=int(input("Ingrese un numero entero:"))
+numero2=int(input("Ingrese otro numero entero:"))
+
+if numero > numero2 :
+    print(numero, "es mayor que",numero2)
+elif numero < numero2 :
+    print(numero, "es menor que",numero2)
+else : 
+    print(numero2,"es igual a ", numero)
