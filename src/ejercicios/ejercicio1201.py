307e57154815e5fb5f2c69462402ead16f015f9b
unknown <guillermoavalle9@gmail.com>
2026-06-19 19:47:40 -0300

    En el ejercicio 1201 use los signos para operaciones numericas para resolver las operaciones requeridas de cada print.

---
+++ src/ejercicios/ejercicio1201.py
@@ -0,0 +1,11 @@
+num1 = int(input())
+num2 = int(input())
+
+print("La suma de los dos numeros es:", num1 + num2)
+print("La resta del primer numero menos el segundo es:", num1 - num2)
+print("La multiplicacion de los dos numeros es:", num1 * num2)
+print("La division del primer numero entre el segundo es:", num1 / num2)
+print("El resto de la division del primer numero entre el segundo es:", num1 % num2)
+print("La division entera del primer numero entre el segundo es:", num1 // num2)
+print("El valor absoluto del primer numero es:", abs(num1))
+print("El valor absoluto del segundo numero es:", abs(num2))
