be9f3bea40b29b1b967926bebf7573cf270155c8
Ludmila Ravichoni <ludmiravichoni@gmail.com>
2026-06-20 23:39:29 -0300

    correccion ejercicio 1201. agregue una condicion para verificar que el segundo numero en la division sea cero,de esta manera no se hace la division y salta un mensaje

--- src/ejercicios/ejercicio1201.py
+++ src/ejercicios/ejercicio1201.py
@@ -11,7 +11,10 @@ nro2 = int(input("Ingrese el segundo numero:"))
 print("La suma de los dos numeros es:",nro1+nro2)
 print("La resta del primer numero menos el segundo es:", nro1-nro2)
 print("La multiplcacion de los dos numeros es:", nro1*nro2)
-print("La division del primer numero entre el segundo es:", nro1/nro2)
+if nro2 != 0:
+    print("La division del primer numero entre le segundo es:", nro1/nro2)
+else:
+    print("No se puede dividir por cero")
 print("El resto de la division del primer numero entre el segundo es:", nro1%nro2)
 print("La division entera del primer numero entre el segundo es:", nro1//nro2)
 print("El valor absoluto del primer numero es:",nro1)
