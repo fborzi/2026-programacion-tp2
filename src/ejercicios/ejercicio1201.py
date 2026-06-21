ee7b50b1afc497b92ddc2a0fabe8f27a87b06ac9
Ludmila Ravichoni <ludmiravichoni@gmail.com>
2026-06-20 23:50:53 -0300

    correccion ejercicio 1201, uni la division, el resto de la division y la division entera en una minsma estructura de condicion y agregue el abs para mostrar el valor absoluto

--- src/ejercicios/ejercicio1201.py
+++ src/ejercicios/ejercicio1201.py
@@ -13,9 +13,9 @@ print("La resta del primer numero menos el segundo es:", nro1-nro2)
 print("La multiplcacion de los dos numeros es:", nro1*nro2)
 if nro2 != 0:
     print("La division del primer numero entre le segundo es:", nro1/nro2)
+    print("El resto de la division del primer numero entre el segundo es:", nro1%nro2)
+    print("La division entera del primer numero entre el segundo es:", nro1//nro2)
 else:
     print("No se puede dividir por cero")
-print("El resto de la division del primer numero entre el segundo es:", nro1%nro2)
-print("La division entera del primer numero entre el segundo es:", nro1//nro2)
-print("El valor absoluto del primer numero es:",nro1)
-print("El valor absoluto del segundo numero es:",nro2)
+print("El valor absoluto del primer numero es:",abs(nro1))
+print("El valor absoluto del segundo numero es:",abs(nro2))
