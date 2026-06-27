c51cb7676b05bee41faeb6d8975f1071914eb3c6
vann <ivanjtomicic@gmai.com>
2026-06-26 21:34:49 -0300

    Correccion del ejercicio 6

--- src/ejercicios/ejercicio1206.py
+++ src/ejercicios/ejercicio1206.py
@@ -4,8 +4,8 @@ cantidad=int(input("ingresa la cantidad de números enteros a procesar:"))
 suma-total = 0
 # bucle para pedir cada número y sumarlo
 for i in range(1,cantidad + 1):
-    num=int(input(f "ingresa el nímero{i}:"))
-    suma_total + = num
+    num=int(input(f"ingresa el nímero{i}:"))
+    suma_total += num
     # es lo mismo que sum_total = sum_total + num
     
     # mostrar el resultado
