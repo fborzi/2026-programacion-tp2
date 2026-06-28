cd76e74396ec0b3461f665d0f56b78fe8f0f9578
Carlos Avalle Vezzoni <avallecarlos1@gmail.com>
2026-06-28 17:52:48 -0300

    [ejercicio1211] dividir positivos/20 en vez de positivos/contadorpos y corregi el mensaje de error. prueba.

--- src/ejercicios/ejercicio1211.py
+++ src/ejercicios/ejercicio1211.py
@@ -8,7 +8,7 @@ contador = 0
 while contador < 20:
     valores = int(input(""))
     if valores < -10 or valores > 10:
-        print("Numero fuera de rango. Intente nuevamente")
+        print("Numero fuera de rango. Intente nuevamente.")
     elif valores < 0:
         negativos = negativos + valores
         contador += 1
@@ -20,6 +20,6 @@ while contador < 20:
         contadorpos = contadorpos + 1
         contador += 1
 
-print("La cantidad de numeros negativos es: ", negativos)
-print("La cantidad de ceros es: ", igualcero)
-print("El promedio de los numeros positivos ingresados es: ", positivos / contadorpos)
+print("La cantidad de numeros negativos es:", negativos)
+print("La cantidad de ceros es:", igualcero)
+print("El promedio de los numeros positivos ingresados es:", positivos / contador)
