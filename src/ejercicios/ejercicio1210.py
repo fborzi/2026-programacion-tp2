531e2f98b9b84d1d1731fb51981673ed4761bb1e
Carlos Avalle Vezzoni <avallecarlos1@gmail.com>
2026-06-28 17:11:07 -0300

    [ejercicio1211] use de while con contador para tener siempre los 20 numeros, acumuladores para negativos y positivos en el if, y validación de rango.

--- src/ejercicios/ejercicio1210.py
+++ src/ejercicios/ejercicio1210.py
@@ -1,4 +1,4 @@
-"escribi un programa que permita ingresar el monto de las ventas de un comercio, el programa debe ir sumando las ventas ingresadas y al finalizar debe mostrar la suma total de las ventas. El programa finaliza cuando se ingresa un 0. Si se ingresa un monto negativo, el programa debe mostrar un mensaje indicando que el monto ingresado es incorrecto y no debe sumarlo a la suma total de las ventas."
+"escribi un programa que permita ingresar el monto de las ventas de un comercio, el programa debe ir sumando las ventas ingresadas y al finalizar debe mostrar la suma total de las ventas."
 ventas = float(input("Ingrese el monto de la venta o ingrese 0 para finalizar: "))
 contador = 0
 
--- src/ejercicios/ejercicio1211.py
+++ src/ejercicios/ejercicio1211.py
@@ -1,1 +1,27 @@
-""
+"escribi un programa que lea 20 numeros enteros entre -10 y 10, y luego muestre la cantidad de numeros negativos, la cantidad de ceros y el promedio de los numeros positivos ingresados."
+
+print("Ingrese 20 numeros enteros entre -10 y 10: ")
+negativos = 0
+igualcero = 0
+positivos = 0
+contadorpos = 0
+contador = 0
+
+while contador < 20:
+    valores = int(input(""))
+    if valores < -10 or valores > 10:
+        print("Numero fuera de rango. Intente nuevamente.")
+    elif valores < 0:
+        negativos = negativos + valores
+        contador += 1
+    elif valores == 0:
+        igualcero = igualcero + 1
+        contador += 1
+    else:
+        positivos = positivos + valores
+        contadorpos = contadorpos + 1
+        contador += 1
+
+print("La cantidad de numeros negativos es: ", negativos)
+print("La cantidad de ceros es: ", igualcero)
+print("El promedio de los numeros positivos ingresados es: ", positivos / contadorpos)
