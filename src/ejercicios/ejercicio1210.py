94533cf5b5c0e2dbeef99bbc6faf80be80b1f5c3
Virginia Galiano <virgigal0@gmail.com>
2026-06-29 17:29:50 -0300

    correcciones

--- src/ejercicios/ejercicio1210.py
+++ src/ejercicios/ejercicio1210.py
@@ -1,12 +1,13 @@
 """en este ejercicio se pide ingresar por teclado el monto de cada venta en una tienda en una semana,
 mostrar el monto total y si hay un monto negativo se debe informar sin interrumpir el programa,
 finaliza cuando se ingrese un monto igual a 0"""
-suma = 0 
+suma = 0
 monto = float(input("Ingrese el monto de la venta: "))
-while monto != 0 :
+while monto != 0:
     if monto < 0:
         print("El monto ingresado es negativo.")
     else:
-        suma = suma + monto
-        monto = float(input("Ingrese el monto de la venta: "))
-print ("La suma total de las ventas es:", suma)
+        suma += monto
+
+    monto = float(input("Ingrese el monto de la venta: "))
+print("La suma total de las ventas es:", suma)
