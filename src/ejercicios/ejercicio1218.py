a4965889a322a3ffcac8759a3b772722719a9a4b
Lautaro Tamola <tamolalautaro@gmail.com>
2026-06-30 19:30:27 -0300

    [Tp-002] - {fixed}: se realiza carga del ejercicio 1218

---
+++ src/ejercicios/ejercicio1218.py
@@ -0,0 +1,5 @@
+"""en este ejercicio pediremos el ingreso de una cadena de caracteres y luego la imprimiremos con la primer letra en 
+mayuscula y el s¿resto en minuscula"""
+titulo = input("Ingrese el titulo de su libro favorito: ").lower()
+cadena_caracteres = titulo[0].upper() + titulo[1:]
+print("Tu libro preferido es:", cadena_caracteres)
