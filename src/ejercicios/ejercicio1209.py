1ad1d89eb949d33d0587511faa7f8f8d7fb8f18c
Lautaro Tamola <tamolalautaro@gmail.com>
2026-06-30 17:01:46 -0300

    [Tp-002] - {fixed}: se realiza ejercio 1209 correspondiente a tp002

--- src/ejercicios/ejercicio1209.py
+++ src/ejercicios/ejercicio1209.py
@@ -1,3 +1,19 @@
 """En este ejercicio pediremos ingresar un numero de alumno en conjunto con la nota de la evaluacion 
 luego evalaremos si aprobo el examen e imprimiremos cantidad de alumnos aprobados y cantidad desaprobados"""
-alumno = 1
+aprobados = 0
+desaprobados = 0
+
+legajo = int(input("ingrese numero de legajo del alumno: "))
+
+while legajo != 0:
+    nota = int(input("ingrese nota de la evaluacion: "))
+
+    if nota > 4:
+        aprobados += 1
+    else:
+        desaprobados += 1
+
+    legajo = int(input("ingrese numero de legajo del alumno"))
+
+print("La cantidad de aprobados es:", aprobados)
+print("La cantidad de desaprobados es:", desaprobados)
