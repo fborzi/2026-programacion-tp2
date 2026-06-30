69be25dd620ccc585304bbfb7dccd2ddbde0f044
Lautaro Tamola <tamolalautaro@gmail.com>
2026-06-30 20:15:05 -0300

    [Tp-002] - {fix}: se modifica ejercicio 1216 para que no afecten los caracteres en mayuscula

--- src/ejercicios/ejercicio1216.py
+++ src/ejercicios/ejercicio1216.py
@@ -1,7 +1,7 @@
 """ en este ejercicio vamos a pedir el ingreso de una cadena de caracteres y luego un caracter a reemplaza en la cadena
 luego imprimiremos la cadena con el caracter reemplazado"""
 cadena = input("ingrese cadena de caracateres: ").lower()
-caracter = input("ingrese caracter a reemplazar: ")
+caracter = input("ingrese caracter a reemplazar: ").lower()
 cadena_final = ""
 for letra in cadena:
     if letra == caracter:
