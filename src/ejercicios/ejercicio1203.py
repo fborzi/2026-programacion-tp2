83a22f37930d151407b6d05f6968e72db4c66d8e
iaru355 <iaragaray203@gmail.com>
2026-06-30 02:39:39 -0300

    Convert Java program to Python for comparison

--- src/ejercicios/ejercicio1203.py
+++ src/ejercicios/ejercicio1203.py
@@ -1,21 +1,9 @@
-import java.util.Scanner;
+x = int(input())
+y = int(input())
 
-public class Main {
-    public static void main(String[] args) {
-
-        Scanner sc = new Scanner(System.in);
-
-        int x = sc.nextInt();
-        int y = sc.nextInt();
-
-        if (x > y) {
-            System.out.println(x + " es mayor que " + y);
-        } else if (x < y) {
-            System.out.println(x + " es menor que " + y);
-        } else {
-            System.out.println(x + " es igual a " + y);
-        }
-
-        sc.close();
-    }
-}
+if x > y:
+    print(x, "es mayor que", y)
+elif x < y:
+    print(x, "es menor que", y)
+else:
+    print(x, "es igual a", y)
