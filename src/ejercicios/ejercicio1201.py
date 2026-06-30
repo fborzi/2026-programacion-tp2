58c8731c50f33cc11fa317bf1bc3e1ec073e65b4
iaru355 <iaragaray203@gmail.com>
2026-06-30 02:48:40 -0300

    Implement zero division check in ejercicio1201.py
    Added zero division check for division operations.

--- src/ejercicios/ejercicio1201.py
+++ src/ejercicios/ejercicio1201.py
@@ -1,11 +1,16 @@
-num1 = int(input())
+   num1 = int(input())
 num2 = int(input())
 
 print("la suma de los dos numeros es :", num1 + num2)
 print("la resta del primer numero menos el segundo es:", num1 - num2)
 print("la multiplicacion de los dos numeros es :", num1 * num2)
-print("la division del primer numero entre el segundo es :", num1 / num2)
-print("el resto de la division del primer numero entre el segundo es :", num1 % num2)
-print("la division entera del primer numero entre el segundo es :", num1 // num2)
+
+if num2 != 0:
+    print("la division del primer numero entre el segundo es :", num1 / num2)
+    print("el resto de la division del primer numero entre el segundo es :", num1 % num2)
+    print("la division entera del primer numero entre el segundo es :", num1 // num2)
+else:
+    print("no se puede dividir por cero")
+
 print("el valor absoluto del primer numero es :", abs(num1))
-print("el valor absoluto del segundo numero es :", abs(num2))
+print("el valor absoluto del segundo numero es :", abs(num2))                                                                       decime q parte arreglo
