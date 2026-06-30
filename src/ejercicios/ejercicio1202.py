5f7989773f87cc1e7ece0a5bbbfd73369c0f5594
Sofia Bolmeni <aifosd26@gmail.com>
2026-06-30 07:17:55 -0300

    refactor: elimina la variable mensaje y simplifica la salida

--- src/ejercicios/ejercicio1202.py
+++ src/ejercicios/ejercicio1202.py
@@ -5,10 +5,8 @@ al usuario."""
 num = int(input("Ingrese un numero entero: "))
 
 if num > 10:
-    mensaje = "El numero es mayor que 10."
+    print("El numero es mayor que 10.")
 elif num < 10:
-    mensaje = "El numero es menor que 10."
+    print("El numero es menor que 10.")
 else:
-    mensaje = "El numero es igual a 10."
-
-print(mensaje)
+    print("El numero es igual a 10.")
