563dd2d25eecdcd3f10e9cd6297d3ee978c629d5
Sofia Bolmeni <aifosd26@gmail.com>
2026-06-30 06:28:45 -0300

    feat: corrige validación de rango y salida de mensajes fuera de rango,cambia el nombre de la variable promedio para que la tome pylint

--- src/ejercicios/ejercicio1211.py
+++ src/ejercicios/ejercicio1211.py
@@ -6,17 +6,18 @@ los resultados obtenidos."""
 
 suma_negativos = 0
 cantidad_ceros = 0
-cantidad_positivos = 0
 suma_positivos = 0
-cantidad_fuera_rango = 0
+cantidad_positivos = 0
 
-for i in range(20):
+i = 0
 
-    numero = int(input())
+while i < 20:
 
-    while numero < -10 or numero > 10:
-        cantidad_fuera_rango += 1
-        numero = int(input())
+    numero = int(input("Ingrese un numero entre -10 y 10: "))
+
+    if numero < -10 or numero > 10:
+        print("Numero fuera de rango. Intente nuevamente.")
+        continue
 
     if numero < 0:
         suma_negativos += numero
@@ -28,14 +29,11 @@ for i in range(20):
         suma_positivos += numero
         cantidad_positivos += 1
 
-if cantidad_positivos > 0:
-    promedio = suma_positivos / cantidad_positivos
-else:
-    promedio = 0
+    i += 1
 
-for i in range(cantidad_fuera_rango):
-    print("Numero fuera de rango. Intente nuevamente.")
+
+promedio_positivos = suma_positivos / cantidad_positivos
 
 print("La cantidad de numeros negativos es:", suma_negativos)
 print("La cantidad de ceros es:", cantidad_ceros)
-print("El promedio de los numeros positivos ingresados es:", promedio)
+print("El promedio de los numeros positivos ingresados es:", promedio_positivos)
