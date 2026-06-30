36f9bbb01e07ce4187262ba4c15c0b2019317d85
Ludmila Ravichoni <ludmiravichoni@gmail.com>
2026-06-30 09:22:47 -0300

    correccion ejercicio 1205, le saque el else para que no muestre ningun mensaje en caso de no cumplir con el dia ni cantidad de productos

--- src/ejercicios/ejercicio1205.py
+++ src/ejercicios/ejercicio1205.py
@@ -1,7 +1,10 @@
+"""
+El ejercicio 1205 pide que el usuario suba un dia a la semana y la cantidad de articulos comprados, si el dia es lunes y la cantidad de articulos es mas de 3, se le da un descuento 
+utilice una estructura condicional para indicar si el usuario puede acceder al descuento o no y en cualquiera de los dos casos agregue un mensaje para indicar el resultado.
+
+"""
 dia = input("ingrese fecha:")
 producto = int(input("Ingrese articulos comprados:"))
 
 if dia == "lunes" and producto > 3:
     print("Accede al descuento")
-else:
-    print("No accede al descuento")
