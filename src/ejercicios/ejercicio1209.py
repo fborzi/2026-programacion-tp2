08ddd369c5f5671074541dd7e43375128922dd3d
Lautaro Tamola <tamolalautaro@gmail.com>
2026-06-30 17:19:27 -0300

    [Tp-002] - {fixed}: se realiza ejercio 1210 correspondiente a tp002

--- src/ejercicios/ejercicio1209.py
+++ src/ejercicios/ejercicio1209.py
@@ -2,18 +2,13 @@
 luego evalaremos si aprobo el examen e imprimiremos cantidad de alumnos aprobados y cantidad desaprobados"""
 aprobados = 0
 desaprobados = 0
-
 legajo = int(input("ingrese numero de legajo del alumno: "))
-
 while legajo != 0:
     nota = int(input("ingrese nota de la evaluacion: "))
-
-    if nota > 4:
+    if nota >= 4:
         aprobados += 1
     else:
         desaprobados += 1
-
     legajo = int(input("ingrese numero de legajo del alumno"))
-
 print("La cantidad de aprobados es:", aprobados)
 print("La cantidad de desaprobados es:", desaprobados)
---
+++ src/ejercicios/ejercicio1210.py
@@ -0,0 +1,11 @@
+"""En este ejercicio pediremos el ingreso de valor de las ventas semanales y calcularemos el monto total de las mismas, 
+como tambien informaremos si el dato ingresado no corresponde a lo esperado, si es negativo"""
+venta_semanal = 0
+venta = float(input("ingrese monto de la venta: "))
+while venta != 0:
+    if venta > 0:
+        venta_semanal = venta_semanal + venta
+    else:
+        print("el monto ingresado es negativo")
+    venta = float(input("ingrese monto de la venta: "))
+print("la suma del total de las ventas es: ",venta_semanal)
