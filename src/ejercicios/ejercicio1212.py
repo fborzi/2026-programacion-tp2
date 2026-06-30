22b6d3ee4a72034219c4d3b399e9ce4ccb71fa15
PC <gasston327@gmail.com>
2026-06-30 19:31:15 -0300

    Ejerecicio 2012 completo. El usuario ingresa un numero por teclado, y se evalua si es primo o no, para mostrar por consola la respuesta. El numero ingresado debe ser mayot a 1.

---
+++ src/ejercicios/ejercicio1212.py
@@ -0,0 +1,18 @@
+numero = 0
+esPrimo = True
+
+numero = int(input("Ingrese un numero entero mayor a 1:"))
+
+if numero > 1 :
+    for n in range (2, numero) :
+        if numero % n == 0 :
+            esPrimo= False
+            break
+        else :
+           esPrimo = True
+    if esPrimo :
+        print("Es primo")
+    else :
+        print("No es primo")
+else :
+    print("El numero debe ser mayor a 1")
