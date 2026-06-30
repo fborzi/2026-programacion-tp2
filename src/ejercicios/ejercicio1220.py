f2223f65c631a4930ba853128de13c2923181c91
Lautaro Tamola <tamolalautaro@gmail.com>
2026-06-30 19:51:04 -0300

    [Tp-002] - {fixed}: se realiza carga del ejercicio 1220

---
+++ src/ejercicios/ejercicio1220.py
@@ -0,0 +1,14 @@
+"""En este ejercicio perdiremos una cadena de caracteres para cifrarla por el metodo 'la cifra de cesar' en la cual mediante
+un alfabeto y un numero de corrimiento de lugares cifraremos una frase"""
+texto = input("ingrese texto a encriptar: ").lower()
+corrimiento = int(input("Ingrese numero de lugares: "))
+alfabeto = "abcdefghijklmnñopqrstuvwxyz"
+cadena = ""
+for caracter in texto:
+    if caracter in alfabeto:
+        posicion = alfabeto.index(caracter)
+        nueva_posicion = (posicion + corrimiento) % 27
+        cadena += alfabeto[nueva_posicion]
+    else:
+        cadena += caracter
+print(cadena)
