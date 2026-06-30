92c1c3f270a338cba415a7389bccde545a090ed5
Lautaro Tamola <tamolalautaro@gmail.com>
2026-06-30 19:07:22 -0300

    [Tp-002] - {fixed}: se realiza ejercio 1216

---
+++ src/ejercicios/ejercicio1216.py
@@ -0,0 +1,11 @@
+""" en este ejercicio vamos a pedir el ingreso de una cadena de caracteres y luego un caracter a reemplaza en la cadena
+luego imprimiremos la cadena con el caracter reemplazado"""
+cadena = input("ingrese cadena de caracateres: ")
+caracter = input("ingrese caracter a reemplazar: ")
+cadena_final = ""
+for letra in cadena:
+    if letra == caracter:
+        cadena_final += "*"
+    else:
+        cadena_final += letra
+print(cadena_final)
