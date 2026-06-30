bfb2830673ee7c0557d83d7d2a77bc12a3ca5246
iaru355 <iaragaray203@gmail.com>
2026-06-30 04:41:14 -0300

    Refactor ejercicio1201.py for clarity and consistency

--- src/ejercicios/ejercicio1201.py
+++ src/ejercicios/ejercicio1201.py
@@ -1,16 +1,13 @@
 num1 = int(input())
 num2 = int(input())
 
-print("la suma de los dos numeros es :", num1 + num2)
-print("la resta del primer numero menos el segundo es:", num1 - num2)
-print("la multiplicacion de los dos numeros es :", num1 * num2)
+print("la suma de los dos numeros es:", num1 + num2)
+print("la resta de los dos numeros es:", num1 - num2)
+print("la multiplicacion de los dos numeros es:", num1 * num2)
 
 if num2 != 0:
-    print("la division de los dos numeros es :", num1 / num2)
-    print("la division entera entre ambos es :", num1 // num2)
-    print("el resto de la division es :", num1 % num2)
+    print("la division de los dos numeros es:", num1 / num2)
+    print("la division entera de los numeros es:", num1 // num2)
+    print("el resto de la division es:", num1 % num2)
 else:
-    print("No se puede dividir por cero")
-
-print("el valor absoluto del primer numero es :", abs(num1))
-print("el valor absoluto del segundo numero es :", abs(num2))
+    print("no se puede dividir por cero")
