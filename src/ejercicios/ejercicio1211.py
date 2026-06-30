42278f66658262ac1584ca3ade67b95edf7f22ce
Lautaro Tamola <tamolalautaro@gmail.com>
2026-06-30 17:54:26 -0300

    [Tp-002] - {fixed}: se realiza ejercio 1212 y se modifica print del ejercico 1211 correspondiente a fuera de rango

--- src/ejercicios/ejercicio1211.py
+++ src/ejercicios/ejercicio1211.py
@@ -19,7 +19,7 @@ for i in range(25):
         if numero < 0:
             suma_negativos = suma_negativos + numero
     else:
-        print("numero fuera de rango")
+        print("Numero fuera de rango. Intente nuevamente.")
         
 print("La sumatoria de los valores negativos es:",suma_negativos)
 print("La cantidad de ceros ingresados es:", cantidad_ceros)
---
+++ src/ejercicios/ejercicio1212.py
@@ -0,0 +1,13 @@
+"""En este ejercicio pediremos al usuario ingresar un nymero mayor a 1 y le retornaremos si el mismo es primo o no
+"""
+numero = 0
+numero = int(input("ingrese numero mayor a 1: "))
+es_primo = True
+for i in range(2, numero):
+    if numero % i == 0:
+        es_primo = False
+        break
+if es_primo:
+    print("El numero", numero, "es PRIMO.")
+else:
+    print("El numero", numero, "NO es PRIMO.")
