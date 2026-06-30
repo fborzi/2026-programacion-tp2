016ac47d222d17ef50a330003ecfd1b75323cf03
Sofia Bolmeni <aifosd26@gmail.com>
2026-06-30 07:13:33 -0300

    refactor: utiliza la variable mensaje para almacenar y mostrar el resultado

--- src/ejercicios/ejercicio1202.py
+++ src/ejercicios/ejercicio1202.py
@@ -1,15 +1,14 @@
-
 """Este programa solicita el ingreso de un número entero y determina si el
-valor ingresado es mayor que 10 o no es mayor a 10, informando el resultado correspondiente
+valor ingresado es mayor que 10 o no es mayor a 10 o es igual a 10, informando el resultado correspondiente
 al usuario."""
 
 num = int(input("Ingrese un numero entero: "))
 
-mensaje = ""
-
 if num > 10:
-    print("El numero es mayor que 10.")
+    mensaje = "El numero es mayor que 10."
 elif num < 10:
-    print("El numero es menor que 10.")
+    mensaje = "El numero es menor que 10."
 else:
-    print("El numero es igual a 10.")
+    mensaje = "El numero es igual a 10."
+
+print(mensaje)
