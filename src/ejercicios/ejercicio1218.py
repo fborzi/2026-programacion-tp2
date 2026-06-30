c36d9d0c0f1c1d489b0f1c359c3c794bdd1ce78b
Lautaro Tamola <tamolalautaro@gmail.com>
2026-06-30 20:10:30 -0300

    [Tp-002] - {fix}: se modifica ejercicio 1218 para que no afecten los espacios en el titulo ingresado

--- src/ejercicios/ejercicio1218.py
+++ src/ejercicios/ejercicio1218.py
@@ -1,5 +1,5 @@
 """en este ejercicio pediremos el ingreso de una cadena de caracteres y luego la imprimiremos con la primer letra en 
 mayuscula y el s¿resto en minuscula"""
-titulo = input("Ingrese el titulo de su libro favorito: ").lower()
+titulo = input("Ingrese el titulo de su libro favorito: ").strip().lower()
 cadena_caracteres = titulo[0].upper() + titulo[1:]
 print(cadena_caracteres)
