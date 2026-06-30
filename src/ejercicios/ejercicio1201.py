48aabbac219c27d9a927e7190f83e3dbe506d374
iaru355 <iaragaray203@gmail.com>
2026-06-30 04:46:41 -0300

    Refactor input and arithmetic operations in ejercicio1201

--- src/ejercicios/ejercicio1201.py
+++ src/ejercicios/ejercicio1201.py
@@ -8,9 +8,6 @@ print(a * b)
 if b != 0:
     print(a // b)
     print(a % b)
-else:
-    print("No se puede dividir por cero")
-    print("No se puede calcular resto")
 
 print(abs(a))
 print(abs(b))
