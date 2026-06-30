7c8bf9034f70ad8c2ad2ad6ec789b546b324019c
Lautaro Tamola <tamolalautaro@gmail.com>
2026-06-30 19:18:06 -0300

    [Tp-002] - {fix}: se inicializa variable es_primo en ejercicio 1213

--- src/ejercicios/ejercicio1213.py
+++ src/ejercicios/ejercicio1213.py
@@ -1,6 +1,7 @@
 """ en este ejercicio pediremos el ingreso de numeros enteros mayores a 1 y corroboraremos si se trata de nuemros primos 
 o no primos, mostraremos la cantidad de numeros primos ingresados"""
 cantidad_primos = 0
+es_primo = False
 numero = int(input("ingrese numero mayor a 1: "))
 while numero != 0:
     es_primo = True
