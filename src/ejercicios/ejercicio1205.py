20f39dc4d5a617b77558bd6df6f491587531b44b
Ludmila Ravichoni <ludmiravichoni@gmail.com>
2026-06-30 08:51:22 -0300

    Correccion ejercicio 1205, modifique el codigo ya que el error era que habia colocado el int al momento de agregar el dia, luego cambie la parte de la cantidad de productos en la esctructura condicional ya que habia puesto que sea menos de tres cuando el ejercicio pedia lo contrario. Por ultimo agregue un else para que en caso de no cumplir con el dia ni cantidad de productos, saltq un mensaje de que no accede al descuento. y le sume un comentario

--- src/ejercicios/ejercicio1205.py
+++ src/ejercicios/ejercicio1205.py
@@ -1,6 +1,7 @@
-dia = int(input("ingrese fecha:"))
+dia = input("ingrese fecha:")
 producto = int(input("Ingrese articulos comprados:"))
 
-if dia == "lunes" and producto <3:
+if dia == "lunes" and producto > 3:
     print("Accede al descuento")
-    
+else:
+    print("No accede al descuento")
