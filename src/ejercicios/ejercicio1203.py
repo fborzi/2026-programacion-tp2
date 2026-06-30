5e8107e984af897ea73309b24b2e0fb2eb9e38f9
iaru355 <iaragaray203@gmail.com>
2026-06-30 04:48:13 -0300

    Refactor comparison output to use f-strings

--- src/ejercicios/ejercicio1203.py
+++ src/ejercicios/ejercicio1203.py
@@ -2,8 +2,8 @@ x = int(input())
 y = int(input())
 
 if x > y:
-    print(x, "es mayor que", y)
+    print(f"{x} es mayor que {y}")
 elif x < y:
-    print(x, "es menor que", y)
+    print(f"{x} es menor que {y}")
 else:
-    print(x, "es igual a", y)
+    print(f"{x} es igual a {y}")
