4c4e1fc8151d9b1559a8b01f4d2cd26329bc301b
PC <gasston327@gmail.com>
2026-06-30 21:35:39 -0300

    Se modificaron validaciones sobre el caracter, para que no se puedan ingresar mas de 1 en la variable caracter. ejercicio 1216

--- src/ejercicios/ejercicio1216.py
+++ src/ejercicios/ejercicio1216.py
@@ -2,8 +2,12 @@ cadena = input("Ingrese una cadena de texto: ")
 caracter = input("Ingrese un caracter: ")
 cadenaNueva = ""
 
-if len(caracter) == 1 :
-    for letra in cadena :
+while len(caracter) != 1 :
+    print("El caracter debe tener una logitud de 1")
+    caracter = input("Ingrese un caracter: ")
+   
+if len(caracter) == 1: 
+     for letra in cadena :
         if letra == caracter :
             letra = "*"
             cadenaNueva= cadenaNueva + letra
