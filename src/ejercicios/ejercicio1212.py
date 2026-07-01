b47baae72927b2514ceeed9050fed05cec18a74a
Ludmila Ravichoni <ludmiravichoni@gmail.com>
2026-06-30 22:19:35 -0300

    ejercicio 1212, tenia mal puesta la parte de los numeros primos, busque como es la formula correcta en google y deje la estructura condicional igual a como la puse al principio.

--- src/ejercicios/ejercicio1212.py
+++ src/ejercicios/ejercicio1212.py
@@ -1,6 +1,12 @@
 num= int(input("Ingrese un numero mayor a 1:"))
 
-if num %1 ==0 and num %num == 0:
- print("El numero",num,"es PRIMO")
+primo = True
+
+for i in range(2,num):
+ if num % i == 0:
+  primo = False
+ 
+if primo:
+    print("El numero",num,"es PRIMO")
 else: 
- print("El numero",num,"no es PRIMO")
+    print("El numero",num,"no es PRIMO")
