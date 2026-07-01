928126d9435d07fef46af8e3f3f2420630db2460
genaro lobosco <loboscogena@gmail.com>
2026-06-30 22:32:40 -0300

    ejercicio 1205 solicitar al usuario que ingrese el día de la semana y su cantidad de de artículos comprados por un cliente

---
+++ src/ejercicios/ejercicio1205.py
@@ -0,0 +1,7 @@
+dia = input("Ingrese el dia de la semana: ").lower()
+
+cantidad = int(input("Ingrese la cantidad de articulos: "))
+
+if dia == "lunes" and cantidad > 3:
+    print("Accede al descuento")
+
