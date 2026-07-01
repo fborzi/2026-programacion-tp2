f6828be3119ad3f7e14cdfe0e77110ee7ef92016
Lautaro Tamola <tamolalautaro@gmail.com>
2026-06-30 22:20:44 -0300

    [Tp-002] - {fix}: se agrega strip en cadena para evitar fallo por espacios de mas en la oracion

--- src/ejercicios/ejercicio1214.py
+++ src/ejercicios/ejercicio1214.py
@@ -1,6 +1,7 @@
 """En este ejercicio pediremos el ingreso de una cadena de caracteres en el cual contaremos cantidad de 
 palabras de la cadeba de caracteres y la cantidad de letras de cada palabra"""
-cadena = input("ingrese cadena de caracteres: ")
+texto = input("ingrese cadena de caracteres: ")
+cadena = texto.strip().lower()
 cantidad_palabras = 0
 letras = 0
 palabra = ""
