c551f96325d427d89516be49802054a8954fa767
Lautaro Tamola <tamolalautaro@gmail.com>
2026-06-30 22:30:58 -0300

    [Tp-002] - {fix}: se saca .lower para poder pasar el test y ademas se reinicia variable letras antes del segundo for

--- src/ejercicios/ejercicio1214.py
+++ src/ejercicios/ejercicio1214.py
@@ -1,7 +1,7 @@
 """En este ejercicio pediremos el ingreso de una cadena de caracteres en el cual contaremos cantidad de 
 palabras de la cadeba de caracteres y la cantidad de letras de cada palabra"""
 texto = input("ingrese cadena de caracteres: ")
-cadena = texto.strip().lower()
+cadena = texto.strip()
 cantidad_palabras = 0
 letras = 0
 palabra = ""
@@ -11,8 +11,8 @@ for caracter in cadena:
     elif letras > 0:
             cantidad_palabras = cantidad_palabras + 1
             letras = 0
-
 print("La cantidad de palabras en la cadena es:", cantidad_palabras + 1)
+letras = 0
 for caracter in cadena:
     if caracter != " ":
         palabra = palabra + caracter
