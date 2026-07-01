5d338d2463bd89d9c70644a5c27478ccf4e176bd
Lautaro Tamola <tamolalautaro@gmail.com>
2026-06-30 23:13:29 -0300

    [Tp-002] - {fix}: se cambian variables por comunes debido a que no le gustan tantas letras en ejercicios 1214

--- src/ejercicios/ejercicio1214.py
+++ src/ejercicios/ejercicio1214.py
@@ -3,23 +3,23 @@ palabras de la cadeba de caracteres y la cantidad de letras de cada palabra"""
 texto = input("ingrese cadena de caracteres: ")
 cadena = texto.strip()
 cant_pala = 0
-letr_pala = 0
-pala_cade = ""
+letra = 0
+palabra = ""
 for caracter in cadena:
     if caracter != " ":
-        letr_pala = letr_pala + 1
-    elif letr_pala > 0:
+        letra = letra + 1
+    elif letra > 0:
             cant_pala = cant_pala + 1
-            letr_pala = 0
+            letra = 0
 print("La cantidad de palabras en la cadena es:", cant_pala + 1)
-letr_pala = 0
+letra = 0
 for caracter in cadena:
     if caracter != " ":
-        pala_cade = pala_cade + caracter
-        letr_pala = letr_pala + 1
-    elif letr_pala > 0:
-            print("La palabra", '"' + pala_cade + '"', "tiene", letr_pala, "letras.")
-            pala_cade = ""
-            letr_pala = 0
-if letr_pala > 0:
-    print("La palabra", '"' + pala_cade + '"', "tiene", letr_pala, "letras.")
+        palabra = palabra + caracter
+        letra = letra + 1
+    elif letra > 0:
+            print("La palabra", '"' + palabra + '"', "tiene", letra, "letras.")
+            palabra = ""
+            letra = 0
+if letra > 0:
+    print("La palabra", '"' + palabra + '"', "tiene", letra, "letras.")
