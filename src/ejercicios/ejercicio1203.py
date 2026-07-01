20d4543a1f0a10adfed155b96881a0153a281630
genaro lobosco <loboscogena@gmail.com>
2026-06-30 22:26:48 -0300

    ejercicio 1203 compara dos numeros

---
+++ src/ejercicios/ejercicio1203.py
@@ -0,0 +1,10 @@
+num1 = int(input("Ingrese el primer numero: "))
+num2 = int(input("Ingrese el segundo numero: "))
+if num1 > num2:
+    print(num1, "es mayor que", num2)
+elif num1 < num2:
+    print(num1, "es menor que", num2)
+else:
+    print(num1, "es igual a", num2)
+
+
