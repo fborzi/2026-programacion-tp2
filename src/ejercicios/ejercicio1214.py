dc2ac6f63d4d64c9de15bc2446f74765bb52c8b8
Lautaro Tamola <tamolalautaro@gmail.com>
2026-06-30 23:06:11 -0300

    [Tp-002] - {fix}: se cambian variables tipo snake_case como recomendaba el pylint en ejercicios 1214

--- src/ejercicios/ejercicio1214.py
+++ src/ejercicios/ejercicio1214.py
@@ -2,24 +2,24 @@
 palabras de la cadeba de caracteres y la cantidad de letras de cada palabra"""
 texto = input("ingrese cadena de caracteres: ")
 cadena = texto.strip()
-cantidad_palabras = 0
-letras_palabra = 0
-palabra_cadena = ""
+cant_palabras = 0
+letra_palabr = 0
+palab_caden = ""
 for caracter in cadena:
     if caracter != " ":
-        letras_palabra = letras_palabra + 1
-    elif letras_palabra > 0:
-            cantidad_palabras = cantidad_palabras + 1
-            letras_palabra = 0
-print("La cantidad de palabras en la cadena es:", cantidad_palabras + 1)
-letras_palabra = 0
+        letra_palabr = letra_palabr + 1
+    elif letra_palabr > 0:
+            cant_palabras = cant_palabras + 1
+            letra_palabr = 0
+print("La cantidad de palabras en la cadena es:", cant_palabras + 1)
+letra_palabr = 0
 for caracter in cadena:
     if caracter != " ":
-        palabra_cadena = palabra_cadena + caracter
-        letras_palabra = letras_palabra + 1
-    elif letras_palabra > 0:
-            print("La palabra", '"' + palabra_cadena + '"', "tiene", letras_palabra, "letras.")
-            palabra_cadena = ""
-            letras_palabra = 0
-if letras_palabra > 0:
-    print("La palabra", '"' + palabra_cadena + '"', "tiene", letras_palabra, "letras.")
+        palab_caden = palab_caden + caracter
+        letra_palabr = letra_palabr + 1
+    elif letra_palabr > 0:
+            print("La palabra", '"' + palab_caden + '"', "tiene", letra_palabr, "letras.")
+            palab_caden = ""
+            letra_palabr = 0
+if letra_palabr > 0:
+    print("La palabra", '"' + palab_caden + '"', "tiene", letra_palabr, "letras.")
